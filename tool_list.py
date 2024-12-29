0dbe8e278c8beee6f9fe7aecb08d5fb779a7f43d
Abhishek Kanti <114488605+Abhishek-Kanti@users.noreply.github.com>
2024-12-30 05:23:54 +0530

    firebase

--- tool_list.py
+++ tool_list.py
@@ -19,30 +19,25 @@ import httpx
 import firebase_admin
 from firebase_admin import credentials, db
 
-os.environ["type"]= os.getenv('type')
-os.environ["project_id"]= os.getenv("project_id")
-os.environ["private_key_id"]= os.getenv("private_key_id")
-os.environ["private_key"]= os.getenv("private_key")
-os.environ["client_email"]= os.getenv("client_email")
-os.environ["auth_uri"]= os.getenv("auth_uri")
-os.environ["client_id"]= os.getenv("client_id")
-os.environ["token_uri"]= os.getenv("token_uri")
-os.environ["auth_provider_x509_cert_url"]= os.getenv("auth_provider_x509_cert_url")
-os.environ["client_x509_cert_url"]= os.getenv("client_x509_cert_url")
-os.environ["universe_domain"]= os.getenv("universe_domain")
+os.environ["PROJECT_ID"] = os.getenv("PROJECT_ID")
+os.environ["PRIVATE_KEY_ID"] = os.getenv("PRIVATE_KEY_ID")
+os.environ["PRIVATE_KEY"] = os.getenv("PRIVATE_KEY")
+os.environ["CLIENT_EMAIL"] = os.getenv("CLIENT_EMAIL")
+os.environ["CLIENT_ID"] = os.getenv("CLIENT_ID")
+os.environ["CLIENT_X509_CERT_URL"] = os.getenv("CLIENT_X509_CERT_URL")
 
 DATA={
-        "type": os.environ.get('type'),
-        "project_id": os.environ.get("project_id"),
-        "private_key_id": os.environ.get("private_key_id"),
-        "private_key": os.environ.get("private_key"),
-        "client_email": os.environ.get("client_email"),
-        "auth_uri": os.environ.get("auth_uri"),
-        "client_id": os.environ.get("client_id"),
-        "token_uri": os.environ.get("token_uri"),
-        "auth_provider_x509_cert_url": os.environ.get("auth_provider_x509_cert_url"),
-        "client_x509_cert_url": os.environ.get("client_x509_cert_url"),
-        "universe_domain": os.environ.get("universe_domain")
+        "type": 'service_account',
+        "project_id": os.environ.get("PROJECT_ID"),
+        "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("PRIVATE_KEY"),
+        "client_email": os.getenv("CLIENT_EMAIL"),
+        "auth_uri": 'https://accounts.google.com/o/oauth2/auth',
+        "client_id": os.environ.get("CLIENT_ID"),
+        "token_uri": 'https://oauth2.googleapis.com/token',
+        "auth_provider_x509_cert_url": 'https://www.googleapis.com/oauth2/v1/certs',
+        "client_x509_cert_url": os.environ.get("CLIENT_X509_CERT_URL"),
+        "universe_domain": 'googleapis.com'
 }
     
 cred = credentials.Certificate(DATA)
